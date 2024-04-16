1593b2e504c24d80419ad94fa2aee525fd505127
54oldman <113379055+54oldman@users.noreply.github.com>
2024-04-16 18:47:13 +0300

    first commit

---
+++ src/IntegerGenerator.java
@@ -0,0 +1,19 @@
+public class IntegerGenerator {
+    Random random = new Random();
+
+    public int generate(int min, int max) {
+        int randomNumber = random.nextInt(max - min) + min;
+        return randomNumber;
+    }
+
+
+    public int[] generateNumbers(int numberOfElements, int min, int max) {
+        int[] generatedNumbers = new int[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            generatedNumbers[i] = generate(min, max);
+        }
+        return generatedNumbers;
+    }
+
+
+}
